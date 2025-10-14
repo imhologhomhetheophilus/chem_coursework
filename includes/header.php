c7ffd0ff341ddf9331f1637ade2b45c5b4f8bb0a
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2025-10-14 17:19:30 +0100

    fix links for render

--- includes/header.php
+++ includes/header.php
@@ -36,39 +36,39 @@ href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
 
 </head>
 <body class="bg-light"><nav class="navbar navbar-expand-lg navbar-dark bg-primary fs-4 p-4">
-<div class="container"><a class="navbar-brand fs-2" href="/chem_coursework/index.php">CHE-Dept</a>
+<div class="container"><a class="navbar-brand fs-2" href="chem_coursework/index.php">CHE-Dept</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav"><span class="navbar-toggler-icon"  aria-label="navbar-toggler" aria-labelledby="jobjacob" aria-label="Aria Name"></span></button>
 <div class="collapse navbar-collapse" id="nav"><ul class="navbar-nav ms-auto">
-<li class="nav-item"><a class="nav-link text-light  " href="/chem_coursework/about.php">About</a></li>
-<li class="nav-item  "><a class="nav-link text-light " href="/chem_coursework/personnel.php">Personnnels</a></li>
+<li class="nav-item"><a class="nav-link text-light  " href="chem_coursework/about.php">About</a></li>
+<li class="nav-item  "><a class="nav-link text-light " href="chem_coursework/personnel.php">Personnnels</a></li>
 <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Department Laboratories
           </a>
           <ul class="dropdown-menu">
-            <li><a  class="dropdown-item" href="/chem_coursework/mass_transfer_lab.php" target="_self" rel="noope"  >Mass Transfer Operations</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework/heat_transfer_lab.php"  target="_self" rel="noope">Heat Transfer Operations</a></li>
+            <li><a  class="dropdown-item" href="chem_coursework/mass_transfer_lab.php" target="_self" rel="noope"  >Mass Transfer Operations</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/heat_transfer_lab.php"  target="_self" rel="noope">Heat Transfer Operations</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/chem_coursework/fluid_transfer_lab.php"  target="_self" rel="noope">Fluid and Particles Mechanics</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework/instrument_transfer_lab.php"  target="_self" rel="noope">Instrumentation and Process Control</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework/chemical_reaction_transfer_lab.php"  target="_self" rel="noope">Chemical Reaction Engineering</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework/corrosion_material_transfer_lab.php"  target="_self" rel="noope">Corrosion and Material Science</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework-pro/computer_simulation_transfer_lab.php"  target="_self" rel="noope">Computer Simulation Lab</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/fluid_transfer_lab.php"  target="_self" rel="noope">Fluid and Particles Mechanics</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/instrument_transfer_lab.php"  target="_self" rel="noope">Instrumentation and Process Control</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/chemical_reaction_transfer_lab.php"  target="_self" rel="noope">Chemical Reaction Engineering</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/corrosion_material_transfer_lab.php"  target="_self" rel="noope">Corrosion and Material Science</a></li>
+            <li><a class="dropdown-item" href="chem_coursework-pro/computer_simulation_transfer_lab.php"  target="_self" rel="noope">Computer Simulation Lab</a></li>
             
           </ul>
         </li>
-<li class="nav-item"><a class="nav-link text-light hover:bg-danger " href="/chem_coursework/experiments.php"> Experiments</a></li>
+<li class="nav-item"><a class="nav-link text-light hover:bg-danger " href="chem_coursework/experiments.php"> Experiments</a></li>
 
-<li class="nav-item"><a class="nav-link text-light  " href="/chem_coursework/contacts.php"> Contacts</a></li>
+<li class="nav-item"><a class="nav-link text-light  " href="chem_coursework/contacts.php"> Contacts</a></li>
 <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Mannual
           </a>
           <ul class="dropdown-menu">
-            <li><a  class="dropdown-item" href="/chem_coursework/documents/hnd11.pdf" target="_self" rel="noope"  >HND11</a></li>
-            <li><a class="dropdown-item" href="/chem_coursework/documents/hnd1.pdf"  target="_self" rel="noope">HND1</a></li>
+            <li><a  class="dropdown-item" href="chem_coursework/documents/hnd11.pdf" target="_self" rel="noope"  >HND11</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/documents/hnd1.pdf"  target="_self" rel="noope">HND1</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/chem_coursework/documents/nd11.pdf"  target="_self" rel="noope">ND11</a></li>
+            <li><a class="dropdown-item" href="chem_coursework/documents/nd11.pdf"  target="_self" rel="noope">ND11</a></li>
           </ul>
         </li>
 </ul></div></div></nav><div class="container py-4">
