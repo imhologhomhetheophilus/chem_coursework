054a6dc2551446dd557d2cc7d49c06854aaca91d
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2025-10-15 12:56:06 +0100

    another changes

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "127.0.0.1";   // Use 127.0.0.1 instead of localhost
-$username   = "root";         // Default username for local MySQL
-$password   = "";             // Leave empty if you didn't set a password
-$database   = "chem_coursework"; // Your database name
-$port       = 3306;           // Default MySQL port
+$servername = "db4free.net";
+$username   = "chem_admin";
+$password   = "yourpassword";
+$database   = "chem_coursework";
+$port       = 3306;
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $database, $port);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
