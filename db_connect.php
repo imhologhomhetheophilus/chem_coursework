7429e63993e8d2603e1196a022c3f62ea593e23e
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2025-10-15 12:21:33 +0100

    code change on db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "chem_coursework";
+$servername = "127.0.0.1";   // Use 127.0.0.1 instead of localhost
+$username   = "root";         // Default username for local MySQL
+$password   = "";             // Leave empty if you didn't set a password
+$database   = "chem_coursework"; // Your database name
+$port       = 3306;           // Default MySQL port
 
-$conn = new mysqli($servername, $username, $password, $database);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
