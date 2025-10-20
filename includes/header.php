67a7e516f7e3424ed5a8c3aa001e2619c5b70407
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2025-10-20 16:18:21 +0100

    little changes

--- includes/header.php
+++ includes/header.php
@@ -40,46 +40,46 @@ href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
    <div class=" d-md-flex  m-3 "  data-aos="fade-down">
    <a href="index.php"><img src="/assets/fpn_logo.png" class="img-fluid dep_logo rounded m-3"  alt="logo" style="width:50px"></a>
   </div>
-  <a class="navbar-brand fs-2" href="index.php">CHE-Dept</a>
+  <a class="navbar-brand fs-2" href="/index.php">CHE-Dept</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav"><span class="navbar-toggler-icon"  aria-label="navbar-toggler" aria-labelledby="jobjacob" aria-label="Aria Name"></span></button>
 <div class="collapse navbar-collapse" id="nav"><ul class="navbar-nav ms-auto">
-<li class="nav-item"><a class="nav-link text-light  " href="about.php">About</a></li>
-<li class="nav-item  "><a class="nav-link text-light " href="personnel.php">Personnnels</a></li>
+<li class="nav-item"><a class="nav-link text-light  " href="/about.php">About</a></li>
+<li class="nav-item  "><a class="nav-link text-light " href="/personnel.php">Personnnels</a></li>
 <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Department Laboratories
           </a>
           <ul class="dropdown-menu">
-            <li><a  class="dropdown-item" href="mass_transfer_lab.php" target="_self" rel="noope"  >Mass Transfer Operations</a></li>
-            <li><a class="dropdown-item" href="heat_transfer_lab.php"  target="_self" rel="noope">Heat Transfer Operations</a></li>
+            <li><a  class="dropdown-item" href="/mass_transfer_lab.php" target="_self" rel="noope"  >Mass Transfer Operations</a></li>
+            <li><a class="dropdown-item" href="/heat_transfer_lab.php"  target="_self" rel="noope">Heat Transfer Operations</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="fluid_transfer_lab.php"  target="_self" rel="noope">Fluid and Particles Mechanics</a></li>
-            <li><a class="dropdown-item" href="instrument_transfer_lab.php"  target="_self" rel="noope">Instrumentation and Process Control</a></li>
-            <li><a class="dropdown-item" href="chemical_reaction_transfer_lab.php"  target="_self" rel="noope">Chemical Reaction Engineering</a></li>
-            <li><a class="dropdown-item" href="corrosion_material_transfer_lab.php"  target="_self" rel="noope">Corrosion and Material Science</a></li>
-            <li><a class="dropdown-item" href="computer_simulation_transfer_lab.php"  target="_self" rel="noope">Computer Simulation Lab</a></li>
-            
+            <li><a class="dropdown-item" href="/fluid_transfer_lab.php"  target="_self" rel="noope">Fluid and Particles Mechanics</a></li>
+            <li><a class="dropdown-item" href="/instrument_transfer_lab.php"  target="_self" rel="noope">Instrumentation and Process Control</a></li>
+            <li><a class="dropdown-item" href="/chemical_reaction_transfer_lab.php"  target="_self" rel="noope">Chemical Reaction Engineering</a></li>
+            <li><a class="dropdown-item" href="/corrosion_material_transfer_lab.php"  target="_self" rel="noope">Corrosion and Material Science</a></li>
+            <li><a class="dropdown-item" href="/computer_simulation_transfer_lab.php"  target="_self" rel="noope">Computer Simulation Lab</a></li>
+
           </ul>
         </li>
-<li class="nav-item"><a class="nav-link text-light hover:bg-danger " href="experiments.php"> Experiments</a></li>
+<li class="nav-item"><a class="nav-link text-light hover:bg-danger " href="/experiments.php"> Experiments</a></li>
 
-<li class="nav-item"><a class="nav-link text-light  " href="contacts.php"> Contacts</a></li>
+<li class="nav-item"><a class="nav-link text-light  " href="/contacts.php"> Contacts</a></li>
 <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Mannual
           </a>
           <ul class="dropdown-menu">
-            <li><a  class="dropdown-item" href="documents/hnd11.pdf" target="_self" rel="noope"  >HND11</a></li>
-            <li><a class="dropdown-item" href="documents/hnd1.pdf"  target="_self" rel="noope">HND1</a></li>
+            <li><a  class="dropdown-item" href="/documents/hnd11.pdf" target="_self" rel="noope"  >HND11</a></li>
+            <li><a class="dropdown-item" href="/documents/hnd1.pdf"  target="_self" rel="noope">HND1</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="documents/nd11.pdf"  target="_self" rel="noope">ND11</a></li>
+            <li><a class="dropdown-item" href="/documents/nd11.pdf"  target="_self" rel="noope">ND11</a></li>
           </ul>
         </li>
 </ul></div>
 
    <div class=" d-none  d-md-flex  "  data-aos="fade-left">
 
-   <a href="index.php"><img src="/assets/chem_logo.png" class="img-fluid rounded m-4" alt="logo dept" style=" width:50px"></a>
+   <a href="/index.php"><img src="/assets/chem_logo.png" class="img-fluid rounded m-4" alt="logo dept" style=" width:50px"></a>
 
 
   </div>
