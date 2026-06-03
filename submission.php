674809a3ef3bc9ead281d236a63e3b645176f665
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2026-06-03 18:38:48 +0100

    updating connection

--- submission.php
+++ submission.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . '/includes/db_connect.php';
-require_once __DIR__ . '/includes/auth.php';
+require_once '../includes/db_connect.php';
+require_once '../includes/auth.php';
 if (!isset($_SESSION['admin'])) {
     header('Location: group_login.php');
     exit;
