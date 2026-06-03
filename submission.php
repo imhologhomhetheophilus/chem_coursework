34fbae7277c1c37675ba18a6db2c4be3aeb6bf3b
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2026-06-03 18:27:34 +0100

    connection bug fixed on submission

--- submission.php
+++ submission.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require 'includes/db_connect.php'; // Absolute path from current file
+require_once __DIR__ . '/../includes/db_connect.php';
+require_once __DIR__ . '/../includes/auth.php';
 
 if (!isset($_SESSION['admin'])) {
     header('Location: group_login.php');
