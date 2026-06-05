a5b868394e463c3185eb40b0471f0657a1af85f6
Imhologhomhe Theophilus <imhologhomhetheophilu@gmail.com>
2026-06-05 11:16:56 +0100

    connection bug  fix o

--- submission.php
+++ submission.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'includes/db_connect.php';
 
 if (!isset($_SESSION['group_id'])) {
-    header('Location: group_login.php');
+    header('Location: submission.php');
     exit;
 }
 
